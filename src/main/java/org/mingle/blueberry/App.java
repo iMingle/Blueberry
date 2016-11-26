7345d20763776e9ef5ef5825e939380a5d989286
jinminglei <jinminglei@yeah.net>
2016-11-26 15:46:34 +0800

    create project

---
+++ src/main/java/org/mingle/blueberry/App.java
@@ -0,0 +1,13 @@
+package org.mingle.blueberry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
